94d494bd0359bc018bba60eacea4b65318b04ffd
Túlio Fergulha <Tulio.Fergulha@Ab-Inbev.com>
2023-12-05 20:09:43 -0300

    Refactor package structure

---
+++ go/internal/market/entity/investor.go
@@ -0,0 +1,49 @@
+package entity
+
+type Investor struct {
+	ID            string
+	Name          string
+	AssetPosition []*InvestorAssetPosition
+}
+
+func NewInvestor(id string) *Investor {
+	return &Investor{
+		ID:            id,
+		AssetPosition: []*InvestorAssetPosition{},
+	}
+}
+
+func (i *Investor) AddAssetPosition(assetPosition *InvestorAssetPosition) {
+	i.AssetPosition = append(i.AssetPosition, assetPosition)
+}
+
+func (i *Investor) UpdateAssetPosition(assetID string, shares int) {
+	assetPosition := i.GetAssetPosition(assetID)
+	if assetPosition == nil {
+		i.AssetPosition = append(i.AssetPosition, NewInvestorAssetPosition(assetID, shares))
+	} else {
+		assetPosition.Shares += shares
+	}
+}
+
+func (i *Investor) GetAssetPosition(assetID string) *InvestorAssetPosition {
+	for _, assetPosition := range i.AssetPosition {
+		if assetPosition.AssetId == assetID {
+			return assetPosition
+		}
+	}
+
+	return nil
+}
+
+type InvestorAssetPosition struct {
+	AssetId string
+	Shares  int
+}
+
+func NewInvestorAssetPosition(assetID string, shares int) *InvestorAssetPosition {
+	return &InvestorAssetPosition{
+		AssetId: assetID,
+		Shares:  shares,
+	}
+}
